b13b68dd6271fa5779a319f75d72e7148b860443
Vincent Boutour <bob@vibioh.fr>
2019-01-03 22:33:36 +0100

    Updating script syntax

--- script/index.sh
+++ script/index.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -o errexit
+set -o nounset
+set -o pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-curl -X DELETE \
-  -H "X-Algolia-API-Key: ${ALGOLIA_KEY}" \
-  -H "X-Algolia-Application-Id: ${ALGOLIA_APP}" \
-  "https://${ALGOLIA_APP}.algolia.net/1/indexes/${ALGOLIA_INDEX}"
+main() {
+  curl -X DELETE \
+    -H "X-Algolia-API-Key: ${ALGOLIA_KEY}" \
+    -H "X-Algolia-Application-Id: ${ALGOLIA_APP}" \
+    "https://${ALGOLIA_APP}.algolia.net/1/indexes/${ALGOLIA_INDEX}"
 
-GIT_ROOT=`git rev-parse --show-cdup`
+  GIT_ROOT=$(git rev-parse --show-cdup)
 
-for file in "${GIT_ROOT:-.}/www/doc/"*/index.md; do
-  name=`python -c "import sys; import re; sys.stdout.write(re.compile('([^\/]+)\/index\.md$').search(sys.argv[1]).group(1))" "${file}"`
+  for file in "${GIT_ROOT:-.}/www/doc/"*/index.md; do
+    name=$(python -c "import sys; import re; sys.stdout.write(re.compile('([^\/]+)\/index\.md$').search(sys.argv[1]).group(1))" "${file}")
 
-  "${GIT_ROOT:-.}/bin/algolia" -name "${name}" -source "${file}" -app "${ALGOLIA_APP}" -key "${ALGOLIA_KEY}" -index "${ALGOLIA_INDEX}"
-done
+    "${GIT_ROOT:-.}/bin/algolia" -name "${name}" -source "${file}" -app "${ALGOLIA_APP}" -key "${ALGOLIA_KEY}" -index "${ALGOLIA_INDEX}"
+  done
+}
+
+main "${@}"
