c0fa9744d6f442fae13fd07afcd401a534165e0a
Vincent Boutour <bob@vibioh.fr>
2019-09-02 21:34:38 +0200

    Adding quiet and silent option for curl

--- script/index.sh
+++ script/index.sh
@@ -3,10 +3,12 @@
 set -o nounset -o pipefail -o errexit
 
 main() {
-  curl -X DELETE \
-    -H "X-Algolia-API-Key: ${ALGOLIA_KEY}" \
-    -H "X-Algolia-Application-Id: ${ALGOLIA_APP}" \
-    "https://${ALGOLIA_APP}.algolia.net/1/indexes/${ALGOLIA_INDEX}"
+  curl -q \
+       -sS \
+       -X DELETE \
+       -H "X-Algolia-API-Key: ${ALGOLIA_KEY}" \
+       -H "X-Algolia-Application-Id: ${ALGOLIA_APP}" \
+       "https://${ALGOLIA_APP}.algolia.net/1/indexes/${ALGOLIA_INDEX}"
 
   GIT_ROOT=$(git rev-parse --show-cdup)
 
